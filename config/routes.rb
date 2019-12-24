eb1b9f6bf267bcc6667a4864c12dfdf63231df23
shahadalalmai <shahadalalmai@hotmail.com>
2019-12-24 20:29:22 +0300

    added controllers, models and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  # get 'places/index'
+  # get 'places/show'
+  # get 'places/new'
+  # get 'places/edit'
+
+  resources :places
+  #devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #This is to redefine the default routes and give it the new naming and routing with our new devise named users
+  devise_for :users, controllers: { registrations: 'users/registrations',
+    sessions: 'users/sessions', unlocks: 'users/unlocks',
+    passwords: 'users/passwords', confirmations: 'users/confirmations'}
+
+  root to: "'places#index'"
+
 end
